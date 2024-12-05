8a7fbee46ccc56fbc459a85b9185e350c57bc427
DianaCardozo <125745016+DianaCardozo@users.noreply.github.com>
2024-12-05 08:49:30 -0500

    Taller de ReactNative mediante carga
    Se subió el taller completo

---
+++ proyectoOrdenes/app/(tabs)/itemsMenu.tsx
@@ -0,0 +1,123 @@
+const menuItems = [
+    {
+        nombre: 'Limonada',
+        tipo: 'Bebidas Frías',
+        descripcion: 'Limonada fresca con hielo',
+        precio: 5000,
+        imagen: require('../../assets/images/limonada.png')
+    },
+    {
+        nombre: 'Coca-Cola',
+        tipo: 'Bebidas Frías',
+        descripcion: 'Refresco de cola',
+        precio: 4500,
+        imagen: require('../../assets/images/cocacola.jpg')
+    },
+    {
+        nombre: 'Té Helado',
+        tipo: 'Bebidas Frías',
+        descripcion: 'Té con limón y hielo',
+        precio: 6000,
+        imagen: require('../../assets/images/tehelado.png')
+    },
+    {
+        nombre: 'Agua Mineral',
+        tipo: 'Bebidas Frías',
+        descripcion: 'Agua con gas',
+        precio: 3500,
+        imagen: require('../../assets/images/aguamineral.png')
+    },
+    {
+        nombre: 'Sopa de Pollo',
+        tipo: 'Sopas',
+        descripcion: 'Sopa casera de pollo con fideos',
+        precio: 12000,
+        imagen: require('../../assets/images/sopadepollo.png')
+    },
+    {
+        nombre: 'Sopa de Tomate',
+        tipo: 'Sopas',
+        descripcion: 'Sopa cremosa de tomate',
+        precio: 11000,
+        imagen: require('../../assets/images/sopadetomate.png')
+    },
+    {
+        nombre: 'Sopa de Verduras',
+        tipo: 'Sopas',
+        descripcion: 'Sopa mixta de verduras',
+        precio: 11500,
+        imagen: require('../../assets/images/sopadeverduras.png')
+    },
+    {
+        nombre: 'Sopa de Mariscos',
+        tipo: 'Sopas',
+        descripcion: 'Sopa con una mezcla de mariscos',
+        precio: 15000,
+        imagen: require('../../assets/images/sopademariscos.png')
+    },
+    {
+        nombre: 'Pollo a la Parrilla',
+        tipo: 'Platos del Día',
+        descripcion: 'Pollo con arroz y ensalada',
+        precio: 20000,
+        imagen: require('../../assets/images/polloalaParrilla.png')
+    },
+    {
+        nombre: 'Carne Asada',
+        tipo: 'Platos del Día',
+        descripcion: 'Carne de res con papas fritas',
+        precio: 22000,
+        imagen: require('../../assets/images/carneAsada.png')
+    },
+    {
+        nombre: 'Pasta Alfredo',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Pasta con salsa alfredo y pollo',
+        precio: 18000,
+        imagen: require('../../assets/images/pastaAlfredo.png')
+    },
+    {
+        nombre: 'Pizza Margarita',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Pizza con tomate, mozzarella y albahaca',
+        precio: 25000,
+        imagen: require('../../assets/images/pizzaMargarita.png')
+    },
+    {
+        nombre: 'Hamburguesa',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Hamburguesa con queso y papas fritas',
+        precio: 17000,
+        imagen: require('../../assets/images/hamburguesa.png')
+    },
+    {
+        nombre: 'Tacos',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Tacos con carne, queso y guacamole',
+        precio: 16000,
+        imagen: require('../../assets/images/tacos.png')
+    },
+    {
+        nombre: 'Ensalada César',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Ensalada con pollo y aderezo César',
+        precio: 14000,
+        imagen: require('../../assets/images/cesar.png')
+    },
+    {
+        nombre: 'Risotto de Champiñones',
+        tipo: 'Platos a la Carta',
+        descripcion: 'Risotto cremoso con champiñones',
+        precio: 20000,
+        imagen: require('../../assets/images/risotto.png')
+    },
+    {
+        nombre: 'Menú Infantil',
+        tipo: 'Menú Infantil',
+        descripcion: 'Nuggets de pollo con papas fritas',
+        precio: 12000,
+        imagen: require('../../assets/images/nuggets.png')
+    },
+];
+
+export default menuItems;
